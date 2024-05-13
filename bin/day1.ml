2049df92372b704dd58b4915405534f24478e679
ghost-codes <compounddork@gmail.com>
2024-05-13 18:02:31 +0000

    finished part II of day one

--- bin/day1.ml
+++ bin/day1.ml
@@ -2,19 +2,28 @@
 (* open Stdio *)
 open Core
 
+let replaceIfLess set value =
+  let sorted = List.sort set ~compare:Int.compare in
+  match sorted with
+  | [] -> [ value; 0; 0 ]
+  | [ x ] -> [ value; x; 0 ]
+  | x :: tl -> Int.max value x :: tl
+
 let getInputs =
   let ans =
-    In_channel.fold_lines In_channel.stdin ~init:(0, 0) ~f:(fun acc line ->
+    In_channel.fold_lines In_channel.stdin
+      ~init:(0, [ 0; 0; 0 ])
+      ~f:(fun acc line ->
         match line with
         | "" ->
             let x, y = acc in
-            (0, Int.max x y)
+            (0, replaceIfLess y x)
         | _ ->
             let num = int_of_string line in
             let x, tl = acc in
             (x + num, tl))
   in
   let x, y = ans in
-  Int.max x y
+  List.fold (replaceIfLess y x) ~init:0 ~f:(fun acc x -> acc + x)
 
 let () = printf "Final Answer: %d" getInputs
