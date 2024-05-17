9243b186a2c9554522a58a438ad46c8512ef6dcb
ghost-codes <compounddork@gmail.com>
2024-05-17 19:33:12 +0000

    finish part II of day 4

--- _build/default/bin/day4.ml
+++ _build/default/bin/day4.ml
@@ -1,7 +1,11 @@
 open Core
 
-let find_overlap x y =
-  (x.(0) >= y.(0) && x.(1) <= y.(1)) || (y.(0) >= x.(0) && y.(1) <= x.(1))
+(* let find_total_overlap x y = *)
+(*   (x.(0) >= y.(0) && x.(1) <= y.(1)) || (y.(0) >= x.(0) && y.(1) <= x.(1)) *)
+
+(* 2-4 3-6 *)
+let find_any_overlap x y =
+  if x.(0) >= y.(0) then y.(1) >= x.(0) else x.(1) >= y.(0)
 
 let getInputs =
   In_channel.fold_lines In_channel.stdin ~init:0 ~f:(fun acc line ->
@@ -14,7 +18,7 @@ let getInputs =
       | [] -> acc
       | [ _ ] -> acc
       | hd :: tl :: _ ->
-          if find_overlap (Array.of_list hd) (Array.of_list tl) then acc + 1
+          if find_any_overlap (Array.of_list hd) (Array.of_list tl) then acc + 1
           else acc)
 
 let () = printf "Final answer: %i" getInputs
